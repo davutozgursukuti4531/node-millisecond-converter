a5d5506defc24a76b3caf416532d405e3e06ab1a
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-11-09 15:53:00 +0300

    added secondUnits

--- Utils/units.ts
+++ Utils/units.ts
@@ -1,12 +1,24 @@
 export const msUnits = { 
-  ms = 1,
-  second = ms *  1000,
-  minute = second * 60,
-  hour = minute * 60,
-  day = hour * 24,
-  week = day * 7,
-  month = day * 30,
-  year = month * 12,
-  century = year * 100,
-  millenium = century * 10
+  ms: 1,
+  second: ms *  1000,
+  minute: second * 60,
+  hour: minute * 60,
+  day: hour * 24,
+  week: day * 7,
+  month: day * 30,
+  year: month * 12,
+  century: year * 100,
+  millenium: century * 10
+}
+export const secondUnits = {
+  ms: 1000 / 1,
+  second: 1,
+  minute: 60,
+  hour: minute * 60,
+  day: hour * 24,
+  week: day * 7,
+  month: day * 30,
+  year: month * 12,
+  century: year * 100,
+  millenium: century * 10
 }
