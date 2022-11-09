a1f2ec1eb00df94468440faac4871f8030c62219
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-11-09 15:31:34 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 import numberify from "./Utils/numberify"
+import { msUnits } from "./Utils/units"
+
+
+export const timeToMs = (num: number | string, unit): number => {
+    if(typeof num === "string") num = Number(num)
+    switch(unit){
+      case "ms":
+      case "millisecond":
+        return num
+    }
+}
