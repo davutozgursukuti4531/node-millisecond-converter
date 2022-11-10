aebfe6d31052918c90cc8b31640495bc25924417
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-11-10 21:34:43 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -38,3 +38,40 @@ export const timeToMs = (num: number | string, options: { unit: UnitTypes, conve
             return num * msUnits.millenium
     }
 }
+
+
+export const msToTime = (num: number | string, options?: { unit: UnitTypes }) => {
+    if(typeof num === "string") num = Number(num)
+    switch(options.unit){
+      case "ms":
+      case "millisecond":
+        return num
+      case "s":
+      case "second":
+            return num / msUnits.second
+      case "m":
+      case "minute":
+            return num / msUnits.minute
+      case "h":
+      case "hour":
+            return num / msUnits.hour
+      case "d":
+      case "day":
+            return num / msUnits.day
+      case "w":
+      case "week":
+            return num / msUnits.week
+      case "mo":
+      case "month":
+            return num / msUnits.month
+      case "y":
+      case "year":
+            return num / msUnits.year
+      case "c":
+      case "century":
+            return num / msUnits.century
+      case "mi":
+      case "millenium":
+            return num / msUnits.millenium
+    }
+}
